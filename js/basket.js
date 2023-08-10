ce20b236c0fabb4855fcd804ea5885419ebf6670
Dmitriy <dmitriy.chikish@gmail.com>
2023-08-10 17:23:55 +0200

    fixed function addToBasket

--- js/basket.js
+++ js/basket.js
@@ -1,6 +1,3 @@
-
-
-
 const body = document.querySelector('body');
 const listCard = document.querySelector('.list_card');
 const openBasket = document.querySelector('.open_basket');
@@ -14,53 +11,61 @@ openBasket.addEventListener('click', () => {
   });
 
 function createBasket(item) {
-    const listCard = document.querySelector('.list_card');
-    listCard.innerHTML = '';
-    let card = '';
-  
-    function createBasketProduct(arr) {
+  const listCard = document.querySelector('.list_card');
+  listCard.innerHTML = '';
+  let card = '';
+
+  function createBasketProduct(arr) {
       arr.forEach((product) => {
-        card += `
-          <div class="cards">
-            <ul class="cards_item">
-                <li><img class="img_item" src="${product.img}"></li>
-                <li class="cards_txt">
-                    <p>${product.name}</p>
-                    <span class="price_currency">${product.price}</span>
-                </li>
-            </ul>
-            <ul class="amount">
-                <li class="amount_item">
-                    <button class ="btn_minus" data-action="minus">-</button>
-                    <span id="count" data-counter>1</span>
-                    <button class ="btn_plus" data-action="plus">+</button>
-                </li>
-                <li class="amount_remove">
-                    <p>Remove</p>
-                    <button class="btn_remove" ><img data-action="remove" src="/img/basket/close-circle.png"></button>
-                </li>
-            </ul>
-          </div>`;
+          card += `
+              <div class="cards">
+                  <ul class="cards_item">
+                      <li><img class="img_item" src="${product.img}"></li>
+                      <li class="cards_txt">
+                          <p>${product.name}</p>
+                          <span class="price_currency">${product.price}</span>
+                      </li>
+                  </ul>
+                  <ul class="amount">
+                      <li class="amount_item">
+                          <button class="btn_minus" data-action="minus">-</button>
+                          <span class="count" data-counter="${product.id}">${product.quantity}</span>
+                          <button class="btn_plus" data-action="plus">+</button>
+                      </li>
+                      <li class="amount_remove">
+                          <p>Remove</p>
+                          <button class="btn_remove"><img data-action="remove" src="/img/basket/close-circle.png"></button>
+                      </li>
+                  </ul>
+              </div>`;
       });
       listCard.innerHTML = card;
-  
-    }
-    createBasketProduct(item);
+  }
+  createBasketProduct(item);
 }
+
 let basket = [];
 
 export function addToBasket(product) {
-  basket.push(product);
+  const productIndex = basket.findIndex(item => item.id === product.id);
+  
+  if (productIndex !== -1) {
+      basket[productIndex].quantity += 1;
+  } else {
+      basket.push({ ...product, quantity: 1 });
+  }
+  
   createBasket(basket);
   calcBasketPrice();
 }
+
 
 window.addEventListener('click', (event) => {
     let counter;
     let cards;
     if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
       cards = event.target.closest('.cards');
-      counter = cards.querySelector('[data-counter]');
+      counter = cards.querySelector(`[data-counter="${event.target.closest('.amount_item').querySelector('.count').dataset.counter}"]`)
       const priceEl = cards.querySelector('.price_currency');
       const productPrice = parseFloat(priceEl.innerText);
       const currentCount = parseInt(counter.innerText);
